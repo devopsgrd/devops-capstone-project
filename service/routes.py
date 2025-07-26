063da069d0937b2a5c5cd6fc8223bc6772329015
devopsgrd <grodn.alexandre@gmail.com>
2025-07-26 03:17:12 -0400

    Implement read account endpoint with tests

--- service/routes.py
+++ service/routes.py
@@ -69,6 +69,16 @@ def create_accounts():
 ######################################################################
 
 # ... place you code here to READ an account ...
+@app.route("/accounts/<int:account_id>", methods=["GET"])
+def get_account(account_id):
+    """
+    Reads an Account by ID
+    """
+    app.logger.info(f"Request to read an Account with id: {account_id}")
+    account = Account.find(account_id)
+    if not account:
+        abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
+    return account.serialize(), status.HTTP_200_OK
 
 
 ######################################################################
